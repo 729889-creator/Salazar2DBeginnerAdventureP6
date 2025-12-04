409688e641bb09ab0f55b925084f2c2542a54282
729889-creator <729889@student.dusd.net>
2025-12-04 13:53:25 -0800

    tried to make ducko move
    made ducko move right

--- P6Salazar2DBeginnerAdventure/Assets/Scripts/playercontroller.cs
+++ P6Salazar2DBeginnerAdventure/Assets/Scripts/playercontroller.cs
@@ -1,27 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem
-    ;
-public class playercontroller : MonoBehaviour
-{    public InputAction leftaction;
-    public InputAction moveraction;
-    private Vector2 speed;
 
+
+public class PlayerController : MonoBehaviour
+{
     // Start is called before the first frame update
     void Start()
     {
-        leftaction.Enable();
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = moveraction.ReadValue<Vector2>();
-        Debug.Log(move);
-        
-        Vector2 position =(Vector2) transform.position + move * speed * Time.deltaTime;
-       
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f;
         transform.position = position;
     }
 }
