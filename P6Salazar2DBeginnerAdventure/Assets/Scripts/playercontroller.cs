22c2f02c3eb69123da775a9b2159a612c39561f3
729889-creator <729889@student.dusd.net>
2025-12-05 12:17:59 -0800

    ducko
    tried to make hiim move

--- P6Salazar2DBeginnerAdventure/Assets/Scripts/playercontroller.cs
+++ P6Salazar2DBeginnerAdventure/Assets/Scripts/playercontroller.cs
@@ -1,22 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public InputAction MoveAction;
+
     void Start()
     {
-
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
+        MoveAction.Enable(); // Fix: Add parentheses to call Enable() method
+        Vector3 position = transform.position; // Fix: Use Vector3 instead of Vector2
+        position.x = position.x + 0.1f;
+        transform.position = position;
     }
-
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Vector2 position = (Vector2)transform.position + 0.1f * move * Time.deltaTime; // UNT0024: Re-order operands for better performance
+        Debug.Log(move);
+        position = (Vector2)transform.position + 0.1f * move; // UNT0024: Re-order operands for better performance
         transform.position = position;
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+
+        Debug.Log(horizontal);
+
+        Vector2 tempPosition = transform.position; // Fix: Use a different variable name to avoid redeclaration
+        tempPosition.x = tempPosition.x + 0.1f;
+        transform.position = tempPosition;
     }
 }
